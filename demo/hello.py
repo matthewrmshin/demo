fac3a8645407d43b9c53a716c846663cc48b310c
Matt Shin <matthew.shin@metoffice.gov.uk>
2020-08-21 15:10:14 +0100

    Move implementation of hello to a function

--- demo/hello.py
+++ demo/hello.py
@@ -1,9 +1,16 @@
 """Classic starter application."""
 
 
+def get_hello(who=None):
+    """Classic starter program."""
+    if not who:
+        who = 'World'
+    return f'Hello {who}!'
+
+
 def main():
     """Classic starter program."""
-    print('Hello World!')
+    print(get_hello())
 
 
 if __name__ == '__main__':
